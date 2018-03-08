472d8deeed2a06a185f1680efff4737fbb58f071
Martijn <martijn@maboc.nl>
2018-03-08 22:41:31 +0100

    statistieken pagina (ongelukkigerwijs meta.php genoemd) toegevoegd

---
+++ meta.php
@@ -0,0 +1,86 @@
+<?php
+  include 'sessionstuff.php';
+  include_once 'logging.php';
+  magdit(2);
+  do_log("Statistieken bekijken");
+?>
+
+<html>
+  <head>
+    <title>
+      ILWC
+    </title>
+  </head>
+  <body >
+<?php
+
+  include 'menu.php';
+
+  $con=mysqli_connect("192.168.2.110", "ilwc", "ilwc", "ilwc");
+  if(! $con){
+    die("Foute boel" . mysqli_error($con));
+  }
+
+  $sql="select count(*) from history";
+  $result=mysqli_query($con, $sql);
+  $row=mysqli_fetch_row($result);
+  printf("Total number of lines in history : %s", $row[0]);
+  printf("<br><br>");
+
+  $sql="select   date(wanneer), 
+                 count(*) 
+        from     history 
+        where    not(wanneer is null)
+        group by date(wanneer) 
+        order by date(wanneer) desc";
+  $result=mysqli_query($con, $sql);
+
+  printf("Bewegingen per dag");
+  printf("<table>");
+  while($row=mysqli_fetch_row($result)) {
+    printf("<tr><td>%s</td><td>%s</td></tr>", $row[0], $row[1]);
+  }
+  printf("</table>");
+
+  printf("<br><br>");
+  $sql="select u.name, t.n
+        from   users u
+                 left join (
+                             select   login_id, 
+                                      count(*) n 
+                             from     history 
+                             where    not(login_id is null)
+                             group by login_id 
+                           ) t
+                   on u.id=t.login_id
+        order by 2 desc";
+  $result=mysqli_query($con, $sql);
+
+  printf("Bewegingen per bekende login");
+  printf("<table>");
+  while($row=mysqli_fetch_row($result)) {
+    printf("<tr><td>%s</td><td>%s</td></tr>", $row[0], $row[1]);
+  }
+  printf("</table>");
+
+  printf("<br><br>");
+  $sql="select   address, 
+                 count(*) n 
+        from     history 
+        where    not(address is null)
+        group by address 
+        having   n>1
+        order by 2 desc";
+  $result=mysqli_query($con, $sql);
+
+  printf("Bezoek per IP-adres");
+  printf("<table>");
+  while($row=mysqli_fetch_row($result)) {
+    printf("<tr><td>%s</td><td>%s</td></tr>", $row[0], $row[1]);
+  }
+  printf("</table>");
+
+  mysqli_close($con);
+?>
+  </body>
+</html>
