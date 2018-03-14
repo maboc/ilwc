d9c38a4791a389fbb6546c737300bacc0e1acd38
Martijn <martijn@maboc.nl>
2018-03-14 17:08:29 +0100

    Het begin van een twitter button gemaakt

--- detail.php
+++ detail.php
@@ -35,6 +35,25 @@
 
 <html>
   <head>
+    <script>
+      window.twttr = (
+                       function(d, s, id) {
+                         var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {};
+                         if (d.getElementById(id)) return t;
+                         js = d.createElement(s);
+                         js.id = id;
+                         js.src = "https://platform.twitter.com/widgets.js";
+                         fjs.parentNode.insertBefore(js, fjs);
+
+                         t._e = [];
+                         t.ready = function(f) {
+                           t._e.push(f);
+                         };
+
+                         return t;
+                       }(document, "script", "twitter-wjs")
+                     );
+    </script>
     <title>
       ILWC
     </title>
@@ -74,6 +93,7 @@
 
 ?>
     </table>
+    <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=zodan" data-size="small">
     <br/><br/>
     <div align="center">
 <?php
